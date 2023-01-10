d9db3433d6c8732e830091afb7579ee333735691
hectorcamp <hectorcamp@gmail.com>
2023-01-10 00:45:44 -0300

    Sintaxe

---
+++ java-anatomia-classes/src/edu/hector/segundasemana/MinhaClasse.java
@@ -0,0 +1,19 @@
+package edu.hector.segundasemana;
+
+public class MinhaClasse {
+
+public static void main (String [] args) {
+    
+    String primeiroNome = "Hector";
+    String segundoNome = "Campos";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
