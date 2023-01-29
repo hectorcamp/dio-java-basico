945503f6680c98538d000f3fb4a3708c9bc3e938
hectorcamp <hectorcamp@gmail.com>
2023-01-29 00:54:34 -0300

    exercicios

--- loops-e-arrays/loops-e-arrays-master/src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
+++ loops-e-arrays/loops-e-arrays-master/src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
@@ -5,20 +5,17 @@ e mostre-os na ordem inversa.
 */
 public class Ex1_OrdemInversa {
     public static void main(String[] args) {
-
+     
         int[] vetor = {-5, -6, 15, 50, 8, 4};
 
-        //System.out.println(vetor.length);
-
-        System.out.print("Vetor: ");
-        int count =0;
+        System.out.println("Vetor: ");
+        int count = 0;
         while(count < (vetor.length)) {
-            System.out.print(vetor[count] + " ");
+            System.out.print(vetor[count] + " " );
             count++;
         }
-
-        System.out.print("\nVetor: ");
-        for(int i = (vetor.length - 1); i >= 0; i --) {
+        System.out.println("\nVetor: ");
+        for(int i = (vetor.length -1); i >= 0; i--){
             System.out.print(vetor[i] + " ");
         }
 
--- loops-e-arrays/loops-e-arrays-master/src/br/com/dio/exercicios/arrays/Ex2_Consoantes.java
+++ loops-e-arrays/loops-e-arrays-master/src/br/com/dio/exercicios/arrays/Ex2_Consoantes.java
@@ -13,17 +13,17 @@ public class Ex2_Consoantes {
 
         String[] consoantes = new String[6];
         int quantidadeConsoantes = 0;
-
+    
         int count = 0;
         do {
             System.out.println("Letra: ");
             String letra = scan.next();
 
-            if ( !(letra.equalsIgnoreCase("a") |
+            if (  !(letra.equalsIgnoreCase("a") |
                     letra.equalsIgnoreCase("e") |
                     letra.equalsIgnoreCase("i") |
                     letra.equalsIgnoreCase("o") |
-                    letra.equalsIgnoreCase("u")) )  {
+                    letra.equalsIgnoreCase("u")) ) {
                 consoantes[count] = letra;
                 quantidadeConsoantes++;
             }
@@ -32,14 +32,13 @@ public class Ex2_Consoantes {
 
         } while(count < consoantes.length);
 
-        System.out.print("Consoantes: ");
-        for ( String consoante  :  consoantes ) {
+        System.out.println("Consoantes: ");
+        for ( String consoante : consoantes) {
             if ( consoante != null)
-                System.out.print(consoante + " ");
+            System.out.print(consoante + " ");
+            
         }
         System.out.println("Quantidade de consoantes: " + quantidadeConsoantes);
-        System.out.println(consoantes.length);
-
     }
 
 }
